0d5f4a3a7e1f74ca5a8e50d4d2e555c8cbd108a7
ryan wetmore <ryanwetmore1014@gmil.com>
2024-06-04 12:17:50 -0400

    dapper exercise 1

---
+++ DapperDemo/IDepartmentRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    public interface IDepartmentRepo
+    {
+        public IEnumerable<Department> GetAllDepartments();
+
+        public void InsertDepartment(string newDepartmentName);
+    }
+}
---
+++ DapperDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperDemo;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var depRepo = new DepartmentRepo(conn);
+
+Console.WriteLine("Enter name of the department you would like to create:");
+var depName = Console.ReadLine(); 
+
+depRepo.InsertDepartment(depName);
+
+var departments = depRepo.GetAllDepartments();
+
+foreach (var dep in departments)
+{
+    Console.WriteLine($"ID: {dep.DepartmentId} | Name: {dep.Name}");
+}
