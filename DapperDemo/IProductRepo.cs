8868b037725c3a23cf939062f2d69e532f334727
ryan wetmore <ryanwetmore1014@gmil.com>
2024-06-04 12:46:31 -0400

    completed dapper exercise

---
+++ DapperDemo/IProductRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetAllProducts();
+
+        public void CreateProduct(string name, double price, int CategoryID, bool onSale, int stockLevel);
+    }
+}
---
+++ DapperDemo/ProductRepo.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public void CreateProduct(string name, double price, int CategoryID, bool onSale, int stockLevel)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID, OnSale, StockLevel) VALUES (@name, @price, @categoryID, @nnSale, @stockLevel", new { name, price, CategoryID, onSale, stockLevel});
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
